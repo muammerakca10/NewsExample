ab182209e1acea3bf7c37649ad6386e1d0ee8665
Muammer Akca <muammerakca10@gmail.com>
2022-09-02 13:35:57 +0300

    Project created

---
+++ NewsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsExample
+//
+//  Created by MAC on 2.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
